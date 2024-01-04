52b5b287b13f5b97733b6139d8716eb049962493
IpErrinio <101106107+IpErrinio@users.noreply.github.com>
2024-01-04 16:40:03 +0100

    scoreboard

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function playGame(player) {
-    let choices = ['Kamień', 'Papier', 'Nożyczki'];
-    let computer = choices[Math.floor(Math.random() * 3)];
+  let choices = ['Kamień', 'Papier', 'Nożyczki'];
+  let computer = choices[Math.floor(Math.random() * 3)];
 
-    let result = '';
+  let result = '';
 
-    if (player === computer) {
-      result = 'Remis';
-    } else if (
-      (player === 'Kamień' && computer === 'Nożyczki') ||
-      (player === 'Kamień' && computer === 'Kamień') ||
-      (player === 'Nożyczki' && computer === 'Papier')
-    ) {
-      result = 'Brawo wygrałeś!!!';
-    } else {
-      result = 'Lul ale noob, Przegrana!';
-    }
-
-    document.getElementById('result').innerText = `Wybrałeś ${player}, Komputer wykłada ${computer}. ${result}`;
+  if (player === computer) {
+    result = 'Remis';
+  } else if (
+    (player === 'Kamień' && computer === 'Nożyczki') ||
+    (player === 'Papier' && computer === 'Kamień') ||
+    (player === 'Nożyczki' && computer === 'Papier')
+  ) {
+    result = 'Brawo wygrałeś!!!';
+    playerScore++;
+  } else {
+    result = 'Lul ale noob, Przegrana!';
+    computerScore++;
   }
+
+  updateScoreboard();
+  document.getElementById('result').innerText = `Wybrałeś ${player}, Komputer wykłada ${computer}. ${result}`;
+}
+
+function updateScoreboard() {
+  document.getElementById('playerScore').innerText = playerScore;
+  document.getElementById('computerScore').innerText = computerScore;
+}
